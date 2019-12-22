d6ec2bdee6d98524a96874ad845e8edbfa2cdade
Miao-Cat <56009509+Miao-Cat@users.noreply.github.com>
2019-12-22 17:33:54 +0800

    Create LeetCode_46_410.py

---
+++ Week 2/id_410/LeetCode_46_410.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def permute(self, nums):
+        res = []
+        def backtrack(nums, tmp):
+            if not nums:
+                res.append(tmp)
+                return 
+            for i in range(len(nums)):
+                backtrack(nums[:i] + nums[i+1:], tmp + [nums[i]])
+        backtrack(nums, [])
+        return res
