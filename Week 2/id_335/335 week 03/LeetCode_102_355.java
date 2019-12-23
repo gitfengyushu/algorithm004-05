1af7ac788c9ed8d197f77429e731d3b61c94d458
cuibaoxian <404265413@qq.com>
2019-12-23 18:46:41 +0800

    Create LeetCode_102_355.java

---
+++ Week 2/id_335/335 week 03/LeetCode_102_355.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+
+	public List<List<Integer>> levelOrder(TreeNode root) {
+		List<List<Integer>> levels = new ArrayList<List<Integer>>();
+		if(root == null){
+			return levels;
+		}
+		Queue<TreeNode> queue = new LinkedList<TreeNode>();
+		int level =0;
+		queue.add(root);
+		
+		while(queue.size()>0){
+            levels.add(new ArrayList<Integer>());
+			
+			int level_length = queue.size();
+			for(int i =0;i<level_length;++i){
+                TreeNode node = queue.remove();
+                levels.get(level).add(node.val);
+				if(node.left != null){
+					queue.add(node.left);
+				}
+				if(node.right != null){
+					queue.add(node.right);
+				}
+			}
+			level ++;
+			
+			
+			
+		}
+		
+		
+		
+		
+		
+		return levels;
+	
+
+	}
+}
